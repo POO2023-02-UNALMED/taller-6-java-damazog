f63888a946722f9f640b411af09a773492f35b59
damazog <damazog@unal.edu.co>
2023-10-02 23:17:12 -0500

    a ver

---
+++ src/main/java/vehiculos/Fabricante.java
@@ -0,0 +1,78 @@
+package vehiculos;
+
+import java.util.HashMap;
+
+import java.util.Map;
+
+public class Fabricante {
+	private String nombre;
+	private Pais pais;
+	private int contador = 0;
+	
+	
+	public Fabricante(String nombre, Pais pais) {
+		this.nombre = nombre;
+		this.pais = pais;
+		
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Pais getPais() {
+		return pais;
+	}
+
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+
+	public int getContador() {
+		return contador;
+	}
+	
+	public void sumContador() {
+		contador++;
+		this.pais.sumContador();
+	}
+	
+	public static String fabricaMayorVentas() {
+	    
+	    Map<Fabricante, Integer> ventasPorFabricante = new HashMap<>();
+
+	    
+	    for (Vehiculo vehiculo : Vehiculo.getTodosLosVehiculos()) {
+	        Fabricante fabricante = vehiculo.getFabricante();
+
+	        if (ventasPorFabricante.containsKey(fabricante)) {
+	        	
+	            int ventasAnteriores = ventasPorFabricante.get(fabricante);
+	            ventasPorFabricante.put(fabricante, ventasAnteriores + 1);
+	        } 
+	        
+	        else {
+	       
+	            ventasPorFabricante.put(fabricante, 1);
+	        }
+	    }
+
+	  
+	    int maxVentas = 0;
+	    Fabricante fabricaMayorVentas = null;
+
+	    for (Map.Entry<Fabricante, Integer> entry : ventasPorFabricante.entrySet()) {
+	        if (entry.getValue() > maxVentas) {
+	            maxVentas = entry.getValue();
+	            fabricaMayorVentas = entry.getKey();
+	        }
+	    }
+
+	    return fabricaMayorVentas.getNombre();
+	}
+
+}
---
+++ src/main/java/vehiculos/Pais.java
@@ -0,0 +1,63 @@
+package vehiculos;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Pais {
+	private String nombre;
+	private int contador;
+
+	public Pais(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getContador() {
+		return contador;
+	}
+	
+	public void sumContador() {
+		contador++;
+	}
+	
+	public static String paisMasVendedor() {
+	    
+		Map<Pais, Integer> ventasPorPais = new HashMap<>();
+
+	  
+	    for (Vehiculo vehiculo : Vehiculo.getTodosLosVehiculos()) { 
+	        Pais paisFabricante = vehiculo.getFabricante().getPais();
+
+	        
+	        if (ventasPorPais.containsKey(paisFabricante)) {
+	            
+	            int ventasAnteriores = ventasPorPais.get(paisFabricante);
+	            ventasPorPais.put(paisFabricante, ventasAnteriores + 1);
+	        } 
+	        
+	        else {
+	        
+	            ventasPorPais.put(paisFabricante, 1);
+	        }
+	    }
+
+	    int maxVentas = 0;
+	    Pais paisMasVendedor = null;
+
+	    for (Map.Entry<Pais, Integer> entry : ventasPorPais.entrySet()) {
+	        if (entry.getValue() > maxVentas) {
+	            maxVentas = entry.getValue();
+	            paisMasVendedor = entry.getKey();
+	        }
+	    }
+
+	    return paisMasVendedor.getNombre();
+	}
+}
